e7f880b34e3a597d863bd7b6e62c7f61d3fcd86c
johnyworld <johnyworld@naver.com>
2021-05-21 14:45:41 +0900

    Rating Component

---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Rating = ({ value, text, color }) => {
+  return (
+    <div className='rating'>
+      
+      {[...Array(5)].map((_, i) => (
+        <span>
+          <i style={{ color }} className={
+            value >= i+1
+              ? 'fas fa-star'
+              : value >= i+0.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+          }/>
+        </span>
+      ))}
+
+      {text &&
+        <span> {text}</span>
+      }
+    </div>
+  )
+}
+
+export default Rating;
