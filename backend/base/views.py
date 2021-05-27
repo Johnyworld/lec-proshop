d3fbc1ca2dc5cab4232c0e3469da8c0401b728e4
johnyworld <johnyworld@naver.com>
2021-05-28 01:20:11 +0900

    Register User

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,12 +3,15 @@ from rest_framework import permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Product, User
 from .products import products
 from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+from django.contrib.auth.hashers import make_password
 
 
 # Create your views here.
@@ -32,6 +35,24 @@ class MyTokenObtainPairView(TokenObtainPairView):
 @api_view(['GET'])
 def getRoutes(request):
   return Response('hello')
+
+
+@api_view(['POST'])
+def registerUser(request):
+  data = request.data
+  try:
+    user = User.objects.create(
+      first_name=data['name'],
+      username=data['email'],
+      email=data['email'],
+      password=make_password(data['password'])
+    )
+    serializer = UserSerializerWithToken(user, many=False)
+    return Response(serializer.data)
+  except:
+    message = {'detail': 'User with this email already exists'}
+    return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
