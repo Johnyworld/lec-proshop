37d7149717fece7eee2b2e35527e023ccaac8011
johnyworld <johnyworld@naver.com>
2021-05-28 00:04:11 +0900

    Cart Screen

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
-import { Message } from '../components/Message';
+import Message from '../components/Message';
 import { addToCart } from '../actions/cartActions';
 
 
@@ -22,10 +22,51 @@ const CartScreen = ({ match, location, history }) => {
     }
   }, [dispatch, productId, qty]);
 
+
   return (
-    <div>
-      Hello CartScreen
-    </div>
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0
+          ? <Message variant='info'>Your cart is empty <Link to='/'>Go back</Link></Message>
+          : <ListGroup variant='flush'>
+              {cartItems.map(item => (
+                <ListGroup.Item key={item.product}>
+                  <Row>
+                    <Col md={2}>
+                      <Image src={item.image} alt={item.name} fluid rounded />
+                    </Col>
+                    <Col md={3}>
+                      <Link to={`/product/${item.product}`}>
+                        {item.name}
+                      </Link>
+                    </Col>
+                    <Col md={2}>
+                      ${item.price}
+                    </Col>
+                    <Col md={3}>
+                      <Form.Control as='select' value={qty} onChange={(e) => dispatch(addToCart(item.product, e.target.value))}>
+                        {[...Array(item.countInStock).keys()].map(x => (
+                          <option key={x+1} value={x+1}>
+                            {x+1}
+                          </option>
+                        ))}
+                      </Form.Control>
+                    </Col>
+                    <Col md={1}>
+                      <Button type='button' variant='light' >
+                        <i className='fas fa-trash'></i>
+                      </Button>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+        }
+      </Col>
+      <Col md={4}>
+      </Col>
+    </Row>
   )
 }
 
