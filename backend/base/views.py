780d83372dc17d798ca0d47204f40bb6db6905d4
johnyworld <johnyworld@naver.com>
2021-05-28 00:54:30 +0900

    User Serializer

--- backend/base/views.py
+++ backend/base/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
 from .products import products
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -14,8 +14,10 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
   def validate(self, attrs):
     data = super().validate(attrs)
 
-    data['username'] = self.user.username
-    data['email'] = self.user.email
+    serializer = UserSerializerWithToken(self.user).data
+
+    for k, v in serializer.items():
+      data[k] = v
 
     return data
 
@@ -28,6 +30,12 @@ class MyTokenObtainPairView(TokenObtainPairView):
 @api_view(['GET'])
 def getRoutes(request):
   return Response('hello')
+
+@api_view(['GET'])
+def getUserProfile(request):
+  user = request.user
+  serializer = UserSerializer(user, many=False)
+  return Response(serializer.data)
 
 @api_view(['GET'])
 def getProducts(request):
